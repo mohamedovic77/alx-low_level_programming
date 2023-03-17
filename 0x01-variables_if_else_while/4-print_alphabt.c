3948abcd784b01ccf6a4078b81b5a1425a3dd6d6
mohamedovic77 <mohamed.eljabboury@gmail.com>
2023-03-17 15:15:32 -0700

    I never thought that it would pay off

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char x;
+for (x = 'a'; x <= 'z'; x++)
+{
+if (x != 'e' && x != 'q')
+{
+putchar(x);
+}
+}
+putchar('\n');
+return (0);
+}
